9e6b5ad5bc89803a70e23b51cf22388e24679e8d
Bill Gathen <bill@billgathen.com>
2024-04-03 08:24:35 -0400

    Extract/delegate to DynamicContentElement

--- components/web-components.js
+++ components/web-components.js
@@ -1,28 +1,44 @@
-class AppendContent extends HTMLElement {
+class DynamicContentElement extends HTMLElement {
   constructor() {
     super();
   }
 
-  textToNodes(text) {
-    return new DOMParser().parseFromString(text, 'text/html').body.childNodes;
+  /**
+   * Fetches the content from the specified URL and updates the target element with the response.
+   * @param {string} url - The URL to fetch the content from.
+   * @param {HTMLElement} target - The target element to update with the fetched content.
+   * @param {boolean} [append=false] - Optional. Specifies whether to append the fetched content to the target element.
+   */
+  getContent(url, target, append = false) {
+    fetch(url).
+      then((response) => response.text()).
+      then((text) => append ? this.#append(text, target) : target.innerHTML = text)
   }
 
-  handleContent(text) {
-    this.textToNodes(text).forEach(node => this.appendChild(node));
+  #append(text, target) {
+    this.#textToNodes(text).forEach(node => target.appendChild(node));
+  }
+
+  #textToNodes(text) {
+    return new DOMParser().parseFromString(text, 'text/html').body.childNodes;
+  }
+}
+
+class AppendContent extends DynamicContentElement {
+  constructor() {
+    super();
   }
 
   connectedCallback() {
     this.querySelector('button').addEventListener('click', () => {
-      fetch(this.dataset.src).
-        then((response) => response.text()).
-        then((text) => this.handleContent(text));
+      this.getContent(this.dataset.src, this, 'append')
     })
   }
 }
 
 customElements.define('append-content', AppendContent)
 
-class ReplaceContent extends HTMLElement {
+class ReplaceContent extends DynamicContentElement {
   constructor() {
     super();
   }
@@ -30,16 +46,14 @@ class ReplaceContent extends HTMLElement {
   connectedCallback() {
     const target = document.querySelector(this.dataset.target);
     this.querySelector('button').addEventListener('click', () => {
-      fetch(this.dataset.src).
-        then((response) => response.text()).
-        then((text) => target.innerHTML = text);
+      this.getContent(this.dataset.src, target);
     })
   }
 }
 
 customElements.define('replace-content', ReplaceContent)
 
-class SelectContent extends HTMLElement {
+class SelectContent extends DynamicContentElement {
   constructor() {
     super();
   }
@@ -48,10 +62,8 @@ class SelectContent extends HTMLElement {
     const target = document.querySelector(this.dataset.target);
     this.querySelector('select').addEventListener('change', (e) => {
       if (e.target.value) {
-        const url = `${this.dataset.src}?${e.target.name}=${e.target.value}`;
-        fetch(url).
-          then((response) => response.text()).
-          then((text) => target.innerHTML = text);
+        const url = `${this.dataset.src}?${e.target.name}=${e.target.value}`
+        this.getContent(url, target);
       } else {
         target.innerHTML = "";
       }
